e992217994756e5a004a5f37b49ac0e8dd57719f
1996-isuru <isurusrimalnt@gmail.com>
2021-09-21 07:19:40 +0530

    connect payment gatway

--- routes/hotelPackages.js
+++ routes/hotelPackages.js
@@ -3,74 +3,96 @@ const hotelPackage = require("../models/hotelPackage");
 let Users = require("../models/hotelPackage");
 
 router.route("/add").post((req, res) => {
-        const name = req.body.name;
-        const description= req.body.description;
-        const price = Number(req.body.price);
-        const facilities = req.body.facilities;
-        const picture = req.body.picture;
+  const name = req.body.name;
+  const description = req.body.description;
+  const price = Number(req.body.price);
+  const facilities = req.body.facilities;
+  const picture = req.body.picture;
 
-        const newHotelPackage = new hotelPackage({
-            name,
-            description,
-            price,
-            facilities,
-            picture,
-        })
+  const newHotelPackage = new hotelPackage({
+    name,
+    description,
+    price,
+    facilities,
+    picture,
+  });
 
-        newHotelPackage.save().then(()=>{
-            res.json("Package Added")
-        }).catch(err=>{
-            console.log(err);
-        })
- 
-})
+  newHotelPackage
+    .save()
+    .then(() => {
+      res.json("Package Added");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 router.route("/").get((req, res) => {
-        hotelPackage.find().then((hotelPackages)=>{
-            res.json(hotelPackages)
-        }).catch(err=>{
-            console.log(err)
-        })
+  hotelPackage
+    .find()
+    .then((hotelPackages) => {
+      res.json(hotelPackages);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-})
+router.route("/update/:id").put(async (req, res) => {
+  let packageId = req.params.id;
+  const { name, description, price, facilities, picture } = req.body;
 
-router.route("/update/:id").put(async(req, res) => {
-        let packageId = req.params.id;
-        const{name,description,price,facilities,picture} = req.body;
+  const updateHotelPackage = {
+    name,
+    description,
+    price,
+    facilities,
+    picture,
+  };
+  const update = await hotelPackage
+    .findByIdAndUpdate(packageId, updateHotelPackage)
+    .then(() => {
+      res.status(200).send({ status: "Package Updated" });
+    })
+    .catch.log(err);
+  console.log(err);
+  res.status(500).send({ status: "Error with updating Data" });
+});
 
-        const updateHotelPackage = {
-            name,
-            description,
-            price,
-            facilities,
-            picture
-        }
-        const update = await hotelPackage.findByIdAndUpdate(packageId , updateHotelPackage)
-            .then(() => {
-            res.status(200).send({status:"Package Updated" })
-            }).catch.log(err);
-            console.log(err);
-            res.status(500).send({status:"Error with updating Data"});
-})
+router.route("/delete/:id").delete(async (req, res) => {
+  let packageId = req.params.id;
+  await hotelPackage
+    .findByIdAndDelete(packageId)
+    .then(() => {
+      res.status(200).send({ status: "Package Deleted" });
+    })
+    .catch.log(err);
+  console.log(err.message);
+  res.status(500).send({ status: "Error with delete Data" });
+});
 
-router.route("/delete/:id").delete(async(req, res) => {
-        let packageId = req.params.id;
-        await hotelPackage.findByIdAndDelete(packageId).then(() => {
-            res.status(200).send({status:"Package Deleted" })
-            }).catch.log(err);
-            console.log(err.message);
-            res.status(500).send({status:"Error with delete Data"});
+router.route("/get/:id").get(async (req, res) => {
+  let packageId = req.params.id;
+  const user = await hotelPackage
+    .findById(packageId)
+    .then((hotelPackage) => {
+      res.status(200).send({ status: "Package Fetched", hotelPackage });
+    })
+    .catch.log(err);
+  console.log(err.message);
+  res.status(500).send({ status: "Error with get Data", error: err.message });
+});
 
-})
-
-router.route("/get/:id").get(async(req, res) => {
-    let packageId = req.params.id;
-    const user =  await hotelPackage.findById(packageId ).then((hotelPackage) => {
-        res.status(200).send({status:"Package Fetched", hotelPackage })
-        }).catch.log(err);
-        console.log(err.message);
-        res.status(500).send({status:"Error with get Data" , error:err.message});
-
-})
-
+//get all packages for tourist home
+router.route("/getpackages").post((req, res) => {
+  Users.find()
+    .exec()
+    .then((result) => {
+      console.log(result);
+      return res.status(409).json({
+        message: "get hotel packgaes",
+        data: result,
+      });
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 // const dotenv = require("dotenv");
+const Stripe = require("stripe");
+
 const app = express();
 require("dotenv").config();
+const PUBLISHABLE_KEY =
+  "pk_test_51Ho62uJixMHAqmvGjSDHopdtU5QxjjB2sjXLCCrxyitYCiWth16kZBskruK9gNOdZAo7tZxkAdSFWE5KxQr6WqJw00IR1No2PY";
+const SECRET_KEY =
+  "sk_test_51Ho62uJixMHAqmvGxoc5jdNsJJWWDZk0sXHldeA6jGHV84rPlemuBinak6I6IKUycnf48rOKHGkf6k1JxPu2dF2600I8YkHRJT";
 
 const PORT = process.env.PORT || 3000;
 
@@ -25,21 +31,41 @@ connection.once("open", () => {
   console.log("Mongodb connetction succesfull");
 });
 
+//stripe
+const stripe = Stripe(SECRET_KEY, { apiVersion: "2020-08-27" });
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1099, //lowest denomination of particular currency
+      currency: "usd",
+      payment_method_types: ["card"], //by default
+    });
+
+    const clientSecret = paymentIntent.client_secret;
+
+    res.json({
+      clientSecret: clientSecret,
+    });
+  } catch (e) {
+    console.log(e.message);
+    res.json({ error: e.message });
+  }
+});
+
 const usersRouter = require("./routes/users.js");
 app.use("/user", usersRouter);
 
 const tourPlanRoutes = require("./routes/touristRoutes/tourPlanRoute/tourDetails.js");
-app.use("/tourplan",tourPlanRoutes);
+app.use("/tourplan", tourPlanRoutes);
 
 //then we run this port
 const hotelPackageRouter = require("./routes/hotelPackages.js");
 app.use("/hotelPackage", hotelPackageRouter);
 
-//then we run this port     
+//then we run this port
 app.listen(PORT, () => {
   console.log(`Server is up and running on port number: ${PORT}`);
 });
 
 ////////////////////
 //https://www.section.io/engineering-education/creating-a-real-time-chat-app-with-react-socket-io-with-e2e-encryption/
-
