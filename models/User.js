8514e5e8ea721dbf7ab71dbabb5a457371a573b2
Ramesh Sanjaya <53003946+Ramesh9969@users.noreply.github.com>
2021-10-05 16:44:01 +0530

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,39 +1,38 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 //create the schema
 const usersSchema = new Schema({
-  userName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match:
-      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  living: {
-    type: String,
-  }, 
-  bio: {
-    type: String,
-  },
-  profilePic: {
-    type: String,
-  },
-  PlanedTourIds: [String],
-});
+    userName : {
+        type : String,
+        required: true 
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password : {
+        type: String,
+        required: true
+    },
+    type : {
+        type: String,
+        required: true
+    },
+    living : {
+        type: String
+    },
+    bio : {
+        type: String
+    },
+    profilePic : {
+        type: String
+    }
 
-const Users = mongoose.model("UsersDetails", usersSchema);
+})
+
+const Users = mongoose.model("UsersDetails",usersSchema);
 
 module.exports = Users;
